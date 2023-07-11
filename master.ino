6fec11093209102326a54eefcbb169b6dde56377
trs4053 <102474740+trs4053@users.noreply.github.com>
2023-07-11 21:13:23 +0600

    Add files via upload

---
+++ master.ino
@@ -0,0 +1,30 @@
+#include <SoftwareSerial.h>
+
+
+void master_send(int slave_address, int wheel_velocity, int corner_angle, SoftwareSerial& Serialx, int enable){
+
+  digitalWrite(enable, HIGH);
+  Serialx.print("I");  //start transmission, next value is slave number
+  Serialx.print(slave_address);  //slave number
+  Serialx.print("A");  //next value is corner Angle
+  Serialx.print(corner_angle);
+  Serialx.print("V");  //next value is wheel Velocity
+  Serialx.print(wheel_velocity);
+  Serialx.print("F");  //end of transmission
++  Serialx.flush();
+}
+
+
+SoftwareSerial serialx(PA10,PA9);
+
+void setup(){
+
+  serialx.begin(300);
+  pinMode(PA15, OUTPUT);
+  //serialx.setTimeout(250);
+  
+}
+
+void loop(){
+  master_send(1, 50, 36, serialx, PA15);
+}
